bdf04105d7a803e6cbed2d1751f75d26ff373f5c
SWB <steven.barnes@swbdevelopment.com>
2021-02-22 00:33:36 -0700

    Added EF configuration for value member on OrderItem entity.

---
+++ GarageSale/GarageSale.Data/Configurations/OrderItemConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using GarageSale.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GarageSale.Data.Configurations
+{
+	public class OrderItemConfiguration : IEntityTypeConfiguration<OrderItem>
+	{
+		public void Configure(EntityTypeBuilder<OrderItem> builder)
+		{
+			builder.OwnsOne(i => i.ItemDetails, d =>
+			{
+				d.WithOwner();
+
+				d.Property(oid => oid.ItemName)
+					.HasMaxLength(50)
+					.IsRequired();
+			});
+		}
+	}
+}

22eb896709ef7754d186314985a101dea8ab7dae
SWB <steven.barnes@swbdevelopment.com>
2021-02-28 11:46:31 -0700

    Added CreateItem page.

---
+++ GarageSale/GarageSale.Site/Pages/Item/CreateItem.razor.cs
@@ -0,0 +1,29 @@
+﻿using GarageSale.Shared.ItemDtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GarageSale.Site.Pages.Item
+{
+	public partial class CreateItem
+	{
+		protected override async Task OnInitializedAsync()
+		{
+			Dto = new ItemDto();
+		}
+		private async void HandleValidSubmit()
+		{
+			Dto = await Service.CreateAsync(Dto);
+			if(Dto is null)
+			{
+				StateHasChanged();
+			}
+			else
+			{
+				Navigation.NavigateTo($"/garage-sale/items/{Dto.Id}");
+			}
+			
+		}
+	}
+}
